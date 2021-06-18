b62c2da6af492b9db2b7882f1b4bc35f1de803f8
juan <juanigalarza98@gmail.com>
2021-06-18 18:32:22 -0300

    routes

--- src/app/config/routes.ts
+++ src/app/config/routes.ts
@@ -6,14 +6,14 @@ import { environment } from "src/environments/environment";
 export const current_route = window.location.pathname;
 
 /** Ruta actual */
-export const route_server = (environment.development) ? 'https://api.repuestosgalarza.local/' : 'https://api.repuestosgalarza.local/';
+export const route_server = (environment.development) ? 'http://192.168.1.16:8000' : 'https://api.repuestosgalarza.local/';
 
 /** Ruta de peticiones al servidor */
 export const route_web = `/${window.location.origin}`;
 
 /** Ruta de peticiones al servidor */
-const protocol_api = (environment.development) ? 'https' : 'https';
-const dir_api = (environment.development) ? 'api.repuestosgalarza.local' : 'api.repuestosgalarza.local';
-const port_api = (environment.development) ? '443' : '443';
+const protocol_api = (environment.development) ? 'http' : 'https';
+const dir_api = (environment.development) ? '192.168.1.16' : 'api.repuestosgalarza.local';
+const port_api = (environment.development) ? '8000' : '443';
 const pathname_api = (environment.development) ? 'api/v1' : 'api/v1'
 export const route_api = `${protocol_api}://${dir_api}:${port_api}/${pathname_api}/`;
